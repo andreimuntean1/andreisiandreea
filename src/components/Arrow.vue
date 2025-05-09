b40a6eea1296e66b510d07dd6f020b1f409024f9
Andrei Muntean <account@andreimuntean.dev>
2025-05-10 00:54:57 +0300

    feat: Add animation for better UX

--- src/components/Arrow.vue
+++ src/components/Arrow.vue
@@ -14,6 +14,19 @@ const props = defineProps({
 </template>
 
 <style scoped>
+a {
+  animation: jump 1.5s ease-out infinite;
+}
+
+@keyframes jump {
+  0%,
+  100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+}
 a:active,
 a:focus {
   outline: none;
