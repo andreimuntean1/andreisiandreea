78e1a0273796be060e39554d79a2b0d870ebc90d
Andrei Muntean <account@andreimuntean.dev>
2025-05-11 00:48:07 +0300

    feat: Make each filename for the generated PDF unique

--- src/utils/toPDF.ts
+++ src/utils/toPDF.ts
@@ -7,13 +7,18 @@ export const toPDF = (message: string, author: string): File => {
   doc.setFont("helvetica", "normal");
   doc.text("De la: " + author, 10, 10);
 
-  doc.setFont("helvetica", "normal");
   doc.setFontSize(14);
   doc.text(doc.splitTextToSize(message, 200), 10, 30);
 
   const blobData = doc.output("blob");
+  const kebab = author
+    .trim()
+    .split(/\s+/)
+    .map((p) => p.toLowerCase())
+    .join("-");
+  const filename = `mesaj-${kebab}.pdf`;
 
-  return new File([blobData], "message.pdf", {
+  return new File([blobData], filename, {
     type: "application/pdf",
     lastModified: Date.now(),
   });
